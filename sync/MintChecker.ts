163f8d7e560343acb6d121fda2e6828f1a8b5c18
Kang <kangjianhu@conflux-chain.org>
2023-02-07 10:32:54 +0800

    eth in contract call

--- sync/MintChecker.ts
+++ sync/MintChecker.ts
@@ -728,7 +728,10 @@ Block Explorer URL: https://stepscan.io/
         console.log(`tx on ethereum, from ${txEthReceiptFrom} to ${txEthTo} , logs count ${txEthLogs.length
         } , status ${txEthReceipt.status} value ${fmtValueInTx}`)
 
-        if (txEth.value.toBigInt() && this.tokenAddr.toLowerCase() === '0xa47f43de2f9623acb395ca4905746496d2014d57') {
+        if (txEth.value.toBigInt() && this.tokenAddr.toLowerCase() === '0xa47f43de2f9623acb395ca4905746496d2014d57'
+            // ethereum WETH tx example :
+            // https://www.oklink.com/zh-cn/eth/tx/0x9aa682839e3c1dca5f8506941b61b0bd5fb3ac36e74bc2ae859715ce53970e86?tab=1
+            && txEthLogs.length === 0) {
             console.log(`It's ETH, value ${fmtValueInTx}`)
             if (txEth.value.toBigInt() < wei) {
                 console.log(`[${this.name}] On ethereum, transfer ETH ${fmtValueInTx} < ${mintV}`)
