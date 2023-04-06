0a787523b6559fa48e4457e945859c3932d9ba84
Kang <kangjianhu@conflux-chain.org>
2023-04-06 11:12:20 +0800

    platon210425_p

--- sync/MintChecker.ts
+++ sync/MintChecker.ts
@@ -149,6 +149,7 @@ export class EventChecker {
     notifyMint = true
     public provider: BaseProvider;
     public zksync_era324_p: BaseProvider;
+    public platon210425_p: BaseProvider;
     public ethereumProvider: BaseProvider;
     public bscProvider: BaseProvider;
     public kavaProvider: BaseProvider;
@@ -230,6 +231,7 @@ Block Explorer URL: https://stepscan.io/
         this.evmOsProvider = ethers.getDefaultProvider('https://eth.bd.evmos.org:8545')
         this.milk2001Provider = ethers.getDefaultProvider('https://rpc-mainnet-cardano-evm.c1.milkomeda.com')
         this.zksync_era324_p = ethers.getDefaultProvider('https://mainnet.era.zksync.io')
+        this.platon210425_p = ethers.getDefaultProvider('https://openapi2.platon.network/rpc')
         this.moonbeam1284Provider = ethers.getDefaultProvider('https://rpc.api.moonbeam.network')
         this.ont58Provider = ethers.getDefaultProvider('https://dappnode3.ont.io:10339')
         this.polygon137Provider = ethers.getDefaultProvider('https://polygon-rpc.com')
@@ -586,6 +588,8 @@ Block Explorer URL: https://stepscan.io/
                         [provider, mpc, mpcSet] = [this.Milkomeda2002Provider, '', {}];
                     } else if (fromChainId == 2001) {
                         [provider, mpc, mpcSet] = [this.milk2001Provider, '', {}];
+                    } else if (fromChainId == 210425) {
+                        [provider, mpc, mpcSet] = [this.platon210425_p, '', {}];
                     } else if (fromChainId == 324) {
                         [provider, mpc, mpcSet] = [this.zksync_era324_p, '', {}];
                     } else if (fromChainId == 106) {
