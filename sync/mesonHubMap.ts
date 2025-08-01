6b6c3a373a985024840bf60abd46a77c3e339cc2
Kang <kangjianhu@conflux-chain.org>
2025-08-01 10:38:13 +0800

    meson hub map

---
+++ sync/mesonHubMap.ts
@@ -0,0 +1,64 @@
+// List of hubId
+export const mesonHubs = {
+	"0x00": {realChainId: 0, name: "  ethereum 20250105"}
+	, "0x01": {realChainId: 0, name: "  arbitrum 20250105"}
+	, "0x02": {realChainId: 0, name: "  bnb smart chain 20250105"}
+	, "0x03": {realChainId: 0, name: " polygon"}
+	, "0x04": {realChainId: 0, name: " optimism"}
+	, "0x05": {realChainId: 0, name: "  avalanche 20250105"}
+	, "0x06": {realChainId: 0, name: "  base 20250105"}
+	, "0x07": {realChainId: 0, name: " linea"}
+	, "0x08": {realChainId: 0, name: " zksync"}
+	, "0x09": {realChainId: 0, name: " scroll"}
+	, "0x0a": {realChainId: 0, name: "  mode 20250105"}
+	, "0x0b": {realChainId: 0, name: "  manta 20250105"}
+	, "0x0c": {realChainId: 0, name: " zklink"}
+	, "0x0d": {realChainId: 0, name: "  core 20250105"}
+	, "0x0e": {realChainId: 0, name: " xlayer"}
+	, "0x0f": {realChainId: 0, name: " mantle"}
+	, "0x10": {realChainId: 0, name: "  merlin 20250105"}
+	, "0x11": {realChainId: 0, name: "  b2 20250105"}
+	, "0x12": {realChainId: 0, name: "  bitlayer 20250105"}
+	, "0x13": {realChainId: 0, name: " bevm"}
+	, "0x14": {realChainId: 0, name: " bb"}
+	, "0x15": {realChainId: 0, name: "  bob 20250105"}
+	, "0x16": {realChainId: 0, name: " opbnb"}
+	, "0x17": {realChainId: 0, name: "  sonic 20250105"}
+	, "0x18": {realChainId: 1030, name: "  cfx 20250105"}
+	, "0x1a": {realChainId: 0, name: "  neox 20250105"}
+	, "0x1b": {realChainId: 0, name: "  soneium 20250105"}
+	, "0x20": {realChainId: 0, name: " kava"}
+	, "0x21": {realChainId: 0, name: " kroma"}
+	, "0x22": {realChainId: 0, name: "  kaia 20250105"}
+	, "0x23": {realChainId: 0, name: " ailayer"}
+	, "0x24": {realChainId: 0, name: " zircuit"}
+	, "0x25": {realChainId: 0, name: " iotex"}
+	, "0x26": {realChainId: 0, name: " zeta"}
+	, "0x27": {realChainId: 0, name: "  taiko 20250105"}
+	, "0x28": {realChainId: 0, name: "  sei 20250105"}
+	, "0x29": {realChainId: 0, name: "  duck 20250105"}
+	, "0x2a": {realChainId: 0, name: "  morph 20241220"}
+	, "0x2b": {realChainId: 0, name: "  exsat 20250105"}
+	, "0x2c": {realChainId: 0, name: "  hemi 20250105"}
+	, "0x2d": {realChainId: 0, name: "  corn 20241206"}
+	, "0x2e": {realChainId: 0, name: "  lisk 20241206"}
+	, "0x2f": {realChainId: 0, name: "  taker 20250105"}
+	, "0x30": {realChainId: 0, name: "  rsk 20250105"}
+	, "0x31": {realChainId: 0, name: "  bera 20250105"}
+	, "0x32": {realChainId: 0, name: "  memecore 20250105"}
+	, "0x33": {realChainId: 0, name: "  goat 20250105"}
+	, "0x34": {realChainId: 0, name: "  plume 20250105"}
+	, "0xa0": {realChainId: 0, name: " (non-evm) bitcoin"}
+	, "0xa1": {realChainId: 0, name: " (non-evm) solana"}
+	, "0xa2": {realChainId: 0, name: " (non-evm) tron"}
+	, "0xa3": {realChainId: 0, name: " (non-evm) ton"}
+	, "0xa4": {realChainId: 0, name: " (non-evm) sui"}
+	, "0xa5": {realChainId: 0, name: " (non-evm) aptos"}
+	, "0xa6": {realChainId: 0, name: " (non-evm) movement"}
+	, "0xb0": {realChainId: 0, name: " (non-evm) rooch"}
+	, "0xf0": {realChainId: 0, name: " sepolia"}
+	, "0xf1": {realChainId: 0, name: " merlin_testnet"}
+	, "0xf2": {realChainId: 0, name: " b2_testnet"}
+	, "0xf3": {realChainId: 0, name: "  hype_testnet 20250105"}
+	, "0xff": {realChainId: 0, name: " aptos_testnet, rooch_testnet"}
+}
